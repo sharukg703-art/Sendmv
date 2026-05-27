f1e0fdbea4f4fef344cdeca4865fe6ea498a43fc
sharukg703-art <sharukg703@gmail.com>
2026-05-27 15:08:08 +0500

    Create rates.js

---
+++ routes/routes/rates.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const axios = require('axios');
+
+router.get('/mvr-inr', async (req, res) => {
+  try {
+    const response = await axios.get(
+      'https://open.er-api.com/v6/latest/MVR'
+    );
+    const rate = response.data.rates.INR;
+    res.json({ rate, from: 'MVR', to: 'INR' });
+  } catch (err) {
+    res.json({ rate: 4.82, from: 'MVR', to: 'INR', fallback: true });
+  }
+});
+
+module.exports = router;
